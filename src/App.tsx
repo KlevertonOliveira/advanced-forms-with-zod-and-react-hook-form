a52acfb759b997236d0fd03223e4a0598f287e83
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-04-13 15:56:53 -0300

    feat: integrate zod with react-hook-form for input validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,76 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const createUserFormSchema = z.object({
+  email: z
+    .string()
+    .nonempty('Email is required')
+    .email('Invalid e-mail format'),
+  password: z.string().min(8, 'Password must have at least 8 characters'),
+});
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>;
 
 export default function App() {
-  const { register, handleSubmit } = useForm();
   const [output, setOutput] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+  });
 
   function createUser(data: any) {
     setOutput(JSON.stringify(data, null, 2));
   }
 
   return (
-    <main className='min-h-screen bg-zinc-900 flex justify-center items-center'>
+    <main className='flex items-center justify-center min-h-screen bg-zinc-900'>
       <form
-        className='flex flex-col gap-6 w-full max-w-sm bg-zinc-800 shadow-md p-10 rounded-md'
+        className='flex flex-col w-full max-w-sm gap-5 p-10 rounded-md shadow-md bg-zinc-800'
         onSubmit={handleSubmit(createUser)}
       >
-        <h2 className='text-center text-white font-bold text-4xl mb-3'>
+        <h2 className='mb-3 text-4xl font-bold text-center text-white'>
           Create an account
         </h2>
         <div className='flex flex-col gap-1'>
-          <label className='text-white text-lg' htmlFor='email'>
+          <label className='text-lg text-white' htmlFor='email'>
             Email:
           </label>
           <input
-            className='bg-zinc-500 border border-zinc-400 text-white px-2 shadow-sm rounded h-10'
+            className='h-10 px-2 text-white border rounded shadow-sm bg-zinc-500 border-zinc-400'
             type='email'
             id='email'
             {...register('email')}
           />
+          {errors.email && (
+            <span className='mt-[2px] text-sm text-red-400'>
+              {errors.email.message}
+            </span>
+          )}
         </div>
         <div className='flex flex-col gap-1'>
-          <label className='text-white text-lg' htmlFor='password'>
+          <label className='text-lg text-white' htmlFor='password'>
             Password:
           </label>
           <input
-            className='bg-zinc-500 border border-zinc-400 text-white px-2 shadow-sm rounded h-10'
+            className='h-10 px-2 text-white border rounded shadow-sm bg-zinc-500 border-zinc-400'
             type='password'
             id='password'
             {...register('password')}
           />
+          {errors.password && (
+            <span className='mt-[2px] text-sm text-red-400'>
+              {errors.password.message}
+            </span>
+          )}
         </div>
         <button
           type='submit'
-          className='bg-emerald-600 text-white rounded h-10 text-lg font-semibold hover:bg-emerald-700 transition-colors'
+          className='h-10 text-lg font-semibold text-white transition-colors rounded bg-emerald-600 hover:bg-emerald-700'
         >
           Submit
         </button>
