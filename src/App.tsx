71fb447622e089311d43b23a32cfb61aee3d873f
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-04-13 17:08:20 -0300

    feat: implement form validation and transformation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-
-const createUserFormSchema = z.object({
-  email: z
-    .string()
-    .nonempty('Email is required')
-    .email('Invalid e-mail format'),
-  password: z.string().min(8, 'Password must have at least 8 characters'),
-});
-
-type CreateUserFormData = z.infer<typeof createUserFormSchema>;
+import { createUserFormSchema } from './schemas/createUserFormSchema';
+import { CreateUserFormData } from './types/CreateUserFormData';
 
 export default function App() {
   const [output, setOutput] = useState('');
@@ -23,19 +14,51 @@ export default function App() {
     resolver: zodResolver(createUserFormSchema),
   });
 
-  function createUser(data: any) {
-    setOutput(JSON.stringify(data, null, 2));
+  function createUser(userData: CreateUserFormData) {
+    setOutput(JSON.stringify(userData, null, 2));
   }
 
   return (
     <main className='flex items-center justify-center min-h-screen bg-zinc-900'>
       <form
-        className='flex flex-col w-full max-w-sm gap-5 p-10 rounded-md shadow-md bg-zinc-800'
+        className='flex flex-col w-full max-w-sm gap-4 p-10 rounded-md shadow-md bg-zinc-800'
         onSubmit={handleSubmit(createUser)}
       >
         <h2 className='mb-3 text-4xl font-bold text-center text-white'>
           Create an account
         </h2>
+        <div className='flex flex-col gap-1'>
+          <label className='text-lg text-white' htmlFor='name'>
+            Name:
+          </label>
+          <input
+            className='h-10 px-2 text-white border rounded shadow-sm bg-zinc-500 border-zinc-400'
+            type='text'
+            id='name'
+            {...register('name')}
+          />
+          {errors.name && (
+            <span className='mt-[2px] text-sm text-red-400'>
+              {errors.name.message}
+            </span>
+          )}
+        </div>
+        <div className='flex flex-col gap-1'>
+          <label className='text-lg text-white' htmlFor='age'>
+            Age:
+          </label>
+          <input
+            className='h-10 px-2 text-white border rounded shadow-sm bg-zinc-500 border-zinc-400'
+            type='number'
+            id='age'
+            {...register('age')}
+          />
+          {errors.age && (
+            <span className='mt-[2px] text-sm text-red-400'>
+              {errors.age.message}
+            </span>
+          )}
+        </div>
         <div className='flex flex-col gap-1'>
           <label className='text-lg text-white' htmlFor='email'>
             Email:
@@ -65,6 +88,22 @@ export default function App() {
           {errors.password && (
             <span className='mt-[2px] text-sm text-red-400'>
               {errors.password.message}
+            </span>
+          )}
+        </div>
+        <div className='flex flex-col gap-1'>
+          <label className='text-lg text-white' htmlFor='confirmPassword'>
+            Confirm Password:
+          </label>
+          <input
+            className='h-10 px-2 text-white border rounded shadow-sm bg-zinc-500 border-zinc-400'
+            type='password'
+            id='confirmPassword'
+            {...register('confirmPassword')}
+          />
+          {errors.confirmPassword && (
+            <span className='mt-[2px] text-sm text-red-400'>
+              {errors.confirmPassword.message}
             </span>
           )}
         </div>
