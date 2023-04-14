3adecf6227ccabab1910b8cb22fe84f3ade43d6b
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-04-14 16:23:26 -0300

    feat: update App to use field arrays with Schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useFieldArray, useForm } from 'react-hook-form';
 import { createUserFormSchema } from './schemas/createUserFormSchema';
 import { CreateUserFormData } from './types/CreateUserFormData';
 
@@ -10,9 +10,19 @@ export default function App() {
     register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm<CreateUserFormData>({
     resolver: zodResolver(createUserFormSchema),
   });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'techs',
+  });
+
+  function addNewTech() {
+    append({ title: '', workload: 0 });
+  }
 
   function createUser(userData: CreateUserFormData) {
     setOutput(JSON.stringify(userData, null, 2));
@@ -107,6 +117,61 @@ export default function App() {
             </span>
           )}
         </div>
+
+        {/* Field Array */}
+        <div className='flex flex-col gap-1'>
+          <label
+            className='flex justify-between text-lg text-white'
+            htmlFor='techs'
+          >
+            Technologies
+            <button
+              className='text-sm text-emerald-500'
+              onClick={addNewTech}
+              type='button'
+            >
+              Adicionar
+            </button>
+          </label>
+          {fields.map((field, index) => (
+            <div key={field.id} className='flex justify-between gap-1'>
+              <div className='flex flex-col flex-1 gap-1'>
+                <input
+                  className='h-10 px-2 text-white border rounded shadow-sm bg-zinc-500 border-zinc-400'
+                  type='text'
+                  id='title'
+                  placeholder='title'
+                  {...register(`techs.${index}.title`)}
+                />
+                {errors.techs?.[index]?.title && (
+                  <span className='mt-[2px] text-sm text-red-400'>
+                    {errors.techs?.[index]?.title?.message}
+                  </span>
+                )}
+              </div>
+              <div className='flex flex-col flex-1 gap-1'>
+                <input
+                  className='w-16 h-10 px-2 text-white border rounded shadow-sm bg-zinc-500 border-zinc-400'
+                  type='number'
+                  id='workload'
+                  {...register(`techs.${index}.workload`)}
+                />
+                {errors.techs?.[index]?.workload && (
+                  <span className='mt-[2px] text-sm text-red-400'>
+                    {errors.techs?.[index]?.workload?.message}
+                  </span>
+                )}
+              </div>
+            </div>
+          ))}
+
+          {errors.techs && (
+            <span className='mt-[2px] text-sm text-red-400'>
+              {errors.techs.message}
+            </span>
+          )}
+        </div>
+
         <button
           type='submit'
           className='h-10 text-lg font-semibold text-white transition-colors rounded bg-emerald-600 hover:bg-emerald-700'
